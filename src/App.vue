1f0b73df535919fb8c00a1e722888326fb27a06d
OlguinJv <olguin.j.vito@gmail.com>
2019-03-20 05:53:25 -0300

    fixes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang='pug'>
 #app
-  img(src='./assets/logo.png')
+  img(src='https://olguin-jv.github.io/Vue-Basics/dist/logo.png')
   h1 Platzi Music
 
   select(v-model="selectedCountry")
