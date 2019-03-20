7030f78ab28fdc16789d1a39650255f46650f050
OlguinJv <olguin.j.vito@gmail.com>
2019-03-20 05:34:22 -0300

    Fin del curso

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+    appname: 'PlatziMusic',
+    apiKey: '77435ecfa5d674aca0bf8736890cf32b',
+    secret: '9996a8ba38469881cda25d2dcd07ba22',
+    registeredTo: 'Olguin-Jv'
+}
+
+
+// Application name	PlatziMusic
+// API key	            77435ecfa5d674aca0bf8736890cf32b
+// Shared secret   	9996a8ba38469881cda25d2dcd07ba22
+// Registered to   	Olguin-Jv
